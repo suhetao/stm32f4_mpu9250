f4d7a1357671d94e6fb15a8730ae6772ba199bcc
suhetao <suhetao@gmail.com>
2015-03-12 17:47:59 +0800

    add hardware schematic and gerber

--- Algorithm/inc/Quaternion.h
+++ Algorithm/inc/Quaternion.h
@@ -29,17 +29,17 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 __inline void Quaternion_Add(float *r, float *a, float *b)
 {
 	r[0] = a[0] + b[0];
-	r[1] = a[0] + b[1];
-	r[2] = a[0] + b[2];
-	r[3] = a[0] + b[3];
+	r[1] = a[1] + b[1];
+	r[2] = a[2] + b[2];
+	r[3] = a[3] + b[3];
 }
 
 __inline void Quaternion_Sub(float *r, float *a, float *b)
 {
 	r[0] = a[0] - b[0];
-	r[1] = a[0] - b[1];
-	r[2] = a[0] - b[2];
-	r[3] = a[0] - b[3];
+	r[1] = a[1] - b[1];
+	r[2] = a[2] - b[2];
+	r[3] = a[3] - b[3];
 }
 
 __inline void Quaternion_Multiply(float *r, float *a, float *b)
